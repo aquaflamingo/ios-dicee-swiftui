b915cd21a684f4d7f491569d76d5d7cf39a7d593
aquaflamingo <16901597+aquaflamingo@users.noreply.github.com>
2022-09-02 13:18:20 -0400

    Initial Commit

---
+++ dicee-swiftui/dicee_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  dicee_swiftuiApp.swift
+//  dicee-swiftui
+//
+//  Created by robertsimoes on 9/2/22.
+//
+
+import SwiftUI
+
+@main
+struct dicee_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
